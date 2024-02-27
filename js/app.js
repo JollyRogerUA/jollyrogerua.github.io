52b4c89a2ddf4046bbcc66d3bdf9f6f385517798
JollyRogerUA <vikrutka.ua@gmail.com>
2024-02-27 21:22:20 +0200

    fix 0.8

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     
     // Set current version
-    let ver = '0.7';
+    let ver = '0.8';
 
     // Get the current date
     let currentDate = new Date().toLocaleDateString('ru-RU', {
@@ -21,13 +21,13 @@ window.onload = function() {
         let filteredData = data.filter(item => item.cc === 'USD' || item.cc === 'EUR' || item.cc === 'GBP' || item.cc === 'PLN');
 
         // Create the table header with the current date
-        let table = '<table><thead><tr>';
-        table += '<th colspan=3>Exchange Rates by NBU for ' + currentDate + ' ver=' + ver + '</th>';
-        table += '</tr><tr>';
+        let table = '<table>';
+        table += '<tr><th colspan=3>Exchange Rates by NBU for ' + currentDate + ' ver=' + ver + '</th></tr>';
+        table += '<tr>';
         table += '<th class="center-align">Currency</th>';
         table += '<th class="center-align">Exchange Rate</th>';
         table += '<th class="center-align">Date</th>';
-        table += '</tr></thead><tbody>';
+        table += '</tr>';
 
         // Iterate over the data and populate the table rows
         filteredData.forEach((item, index) => {
@@ -38,21 +38,21 @@ window.onload = function() {
             table += '</tr>';
         });
 
-        table += '</tbody></table>';
+        table += '</table>';
 
     // Display the table in the HTML page
     document.getElementById('exchangeTable1').innerHTML = table;
     })
     .catch(error => console.error('Error fetching data:', error));
     // --- End of the 1st table ---
-
+/*
     // Fetch JSON data from the second provided URL
     fetch('https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5&cors=true')
     .then(response => response.json())
     .then(data => {
         // Create the table header for the second table
         let table2 = '<table><thead>';
-        table2 += '<th colspan=3>Exchange Rates by PrivatBank for ' + currentDate + ' ver=' + ver + '</th>';
+        table2 += '<th colspan=3 class="center-align">Exchange Rates by PrivatBank for ' + currentDate + ' ver=' + ver + '</th>';
         table2 += '</tr><tr>';
         table2 += '<th class="center-align">Currency</th>';
         table2 += '<th class="center-align">Exchange Rate</th>';
@@ -74,4 +74,6 @@ window.onload = function() {
         document.getElementById('exchangeTable2').innerHTML = table2;
     })
     .catch(error => console.error('Error fetching data:', error));
+*/
+
 }
